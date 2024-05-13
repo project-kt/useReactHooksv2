0c86dd766ab54325c706ed1253183113723a5932
gmoisa <gabriel.moisa@wareplace.com>
2024-05-13 17:46:22 +0200

    Fix netlify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Hero from "./_components/hero";
 import NewHooks from "./_components/new-hooks";
 import PopularHooks from "./_components/popular-hooks";
-import RecentlyUsedHooks from "./_components/recently-used-hooks";
+// import RecentlyUsedHooks from "./_components/recently-used-hooks";
 
 function Index(): React.JSX.Element {
   return (
     <main>
       <Hero />
       <div className="flex flex-col gap-y-10">
-        <RecentlyUsedHooks />
+        {/* <RecentlyUsedHooks /> */}
         <NewHooks />
 
         <PopularHooks />
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -5,7 +5,7 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import useHasMounted from "public/hooks/useHasMounted";
+import useHasMounted from "./hooks/useHasMounted";
 
 export function ModeToggle(): React.JSX.Element | undefined {
   const { theme, setTheme } = useTheme();
